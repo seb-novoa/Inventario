ed8c22dc8381e3ebfd7d4c434579cbb5f3695a5b
Sebastian <35838428+seb-novoa@users.noreply.github.com>
2018-07-04 16:33:17 -0400

    solo muestra los equipos disponibles

--- src/PersonalEquipo/forms.py
+++ src/PersonalEquipo/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 
 from PersonalEquipo.models import PersonalEquipo
+from Equipo.models import Equipo
 
 class RelacionForm(forms.ModelForm):
     class Meta:
         model   =   PersonalEquipo
         fields  =   ( 'equipo', 'fecha_termino', )
+
+    def __init__(self, *args, **kwargs):
+        super(RelacionForm, self).__init__(*args, **kwargs)
+        self.fields['equipo'].queryset   =   Equipo.objects.filter(estado = True)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
