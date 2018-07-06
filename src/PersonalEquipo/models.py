43a5bf2f77fe0bb67656b424c96a076181dd35b4
Sebastian <35838428+seb-novoa@users.noreply.github.com>
2018-07-06 11:56:50 -0400

    Se agrega el modelo PersonalEquipoHistoria

--- src/PersonalEquipo/models.py
+++ src/PersonalEquipo/models.py
@@ -5,9 +5,9 @@ from Equipo.models import Equipo
 from Personal.models import Personas
 
 class PersonalEquipo(models.Model):
-    persona     =   models.ForeignKey(Personas, on_delete = models.SET_NULL, null = True)
-    equipo      =   models.ForeignKey(Equipo, on_delete = models.SET_NULL, null = True)
-    estado      =   models.BooleanField(default = False)
+    persona         =   models.ForeignKey(Personas, on_delete = models.SET_NULL, null = True)
+    equipo          =   models.ForeignKey(Equipo, on_delete = models.SET_NULL, null = True)
+    estado          =   models.BooleanField(default = False)
     fecha_inicio    =   models.DateTimeField(auto_now = False, null = True)
     fecha_termino   =   models.DateTimeField(auto_now = False)
 
@@ -30,3 +30,15 @@ class PersonalEquipo(models.Model):
         self.estado         =   True
 
         super(PersonalEquipo, self).save()
+
+class PersonalEquipoHistoria(models.Model):
+    fecha_entrega   =   models.DateTimeField(auto_now = False, null = True)
+    persona         =   models.ForeignKey(Personas, on_delete = models.SET_NULL, null = True )
+    equipo          =   models.ForeignKey(Equipo, on_delete = models.SET_NULL, null = True)
+    fecha_inicio    =   models.DateTimeField(auto_now = False )
+    fecha_termino   =   models.DateTimeField(auto_now = False )
+    hw              =   models.CharField(max_length = 100 )
+    sw              =   models.CharField(max_length = 100 )
+
+    def __str__(self):
+        return '{0} - {1} entregado entre {2} al {3}'.format(self.persona, self.equipo, self.fecha_inicio.date(), self.fecha_entrega.date())
